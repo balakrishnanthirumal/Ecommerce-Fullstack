442eaef3d55a9dabdb484f15b97e75dcc5ae084b
Balakrishnan T <126885624+balakrishnanthirumal@users.noreply.github.com>
2024-12-09 23:59:05 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -9,9 +9,10 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
+    domain: "https://balexstore-frontend.onrender.com"
   });
 
   return token;
