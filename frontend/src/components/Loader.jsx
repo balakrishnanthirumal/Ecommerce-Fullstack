1d00ccd2d06aca6559481638da5fdb6b12463671
Balakrishnan T <126885624+balakrishnanthirumal@users.noreply.github.com>
2024-12-16 13:47:32 +0530

    Update Loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className=" animate-spin rounded-full h-[100px] w-16 border-t-4 border-pink-500 border-opacity-50"></div>
+    <div className=" animate-spin rounded-full h-[100px] ml-[10rem] w-16 border-t-4 border-pink-500 border-opacity-50"></div>
   );
 };
 export default Loader;
